8867bd0b6a3c522af420c00f8fc3e21b09b9e015
DOAING <hf.dong@outlook.com>
2017-09-27 11:15:01 +0800

    MainActivity测试

--- app/src/main/java/view/MainActivity.java
+++ app/src/main/java/view/MainActivity.java
@@ -73,6 +73,9 @@ public class MainActivity extends AppCompatActivity implements IMainView{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /**
+         *
+         */
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
