b69614e1632a049797dbe1d28ec07261e5e77467
DOAING <hf.dong@outlook.com>
2017-10-24 14:22:13 +0800

    2017/10/12

---
+++ app/src/main/java/bean/Order.java
@@ -0,0 +1,16 @@
+package bean;
+
+/**
+ * 项目名称：Order
+ * 类描述：
+ * 创建人：donghaifeng
+ * 创建时间：2017/10/12 15:40
+ * 修改人：donghaifeng
+ * 修改时间：2017/10/12 15:40
+ * 修改备注：
+ */
+
+public class Order {
+
+
+}
