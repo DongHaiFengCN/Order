12ebecf50802afe6294ec6e819d7f5200f0ffd21
DOAING <hf.dong@outlook.com>
2017-10-27 16:47:15 +0800

    Merge branch 'master' of https://github.com/DongHaiFengCN/Order into develop_dong
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/zm/order/view/SaleActivity.java
+++ app/src/main/java/com/zm/order/view/SaleActivity.java
@@ -1,9 +1,11 @@
 package com.zm.order.view;
 
 import android.os.Bundle;
+import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +13,24 @@ import android.widget.Toast;
 
 import com.zm.order.R;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -53,14 +72,28 @@ public class SaleActivity extends AppCompatActivity {
             public void afterEvent(int event, int result, Object data) {
                 if (data instanceof Throwable) {
                     Throwable throwable = (Throwable) data;
-                    String msg = throwable.getMessage();
-                    Toast.makeText(SaleActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    final String msg = throwable.getMessage();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Toast.makeText(SaleActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
                 } else {
                     if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
 
                         // 验证通过读取数据库
 
-                        Toast.makeText(SaleActivity.this,"验证通过！",Toast.LENGTH_SHORT).show();
+                       runOnUiThread(new Runnable() {
+                           @Override
+                           public void run() {
+
+                               Toast.makeText(SaleActivity.this,"发送验证成功！",Toast.LENGTH_SHORT).show();
+
+                           }
+                       });
 
 
 
@@ -96,7 +129,7 @@ public class SaleActivity extends AppCompatActivity {
                 break;
             case R.id.submitcode:
 
-                if(!TextUtils.isEmpty(etcode.getText().toString())){
+                if(TextUtils.isEmpty(etcode.getText().toString())){
 
                     etcode.setError("验证码不能为空！");
 
@@ -104,8 +137,103 @@ public class SaleActivity extends AppCompatActivity {
 
                     SMSSDK.submitVerificationCode("+86", etAmountphone.getText().toString(), etcode.getText().toString());
 
+                    String result = requestData("https://webapi.sms.mob.com/sms/verify","appkey=21efc5a881e60&phone="+etAmountphone.getText().toString()+"&zone=86&&code="+etcode.getText().toString());
+
+                    MyLog.e(result);
                 }
                 break;
         }
     }
+    /**
+     * 发起https 请求
+     * @param address
+     * @param
+     * @return
+     */
+    public  static String requestData(String address ,String params){
+
+        HttpURLConnection conn = null;
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager(){
+                public X509Certificate[] getAcceptedIssuers(){return null;}
+                public void checkClientTrusted(X509Certificate[] certs, String authType){}
+                public void checkServerTrusted(X509Certificate[] certs, String authType){}
+            }};
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+
+            //ip host verify
+            HostnameVerifier hv = new HostnameVerifier() {
+                public boolean verify(String urlHostName, SSLSession session) {
+                    return urlHostName.equals(session.getPeerHost());
+                }
+            };
+
+            //set ip host verify
+            HttpsURLConnection.setDefaultHostnameVerifier(hv);
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            URL url = new URL(address);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");// POST
+            conn.setConnectTimeout(3000);
+            conn.setReadTimeout(3000);
+            // set params ;post params
+            if (params!=null) {
+                conn.setDoOutput(true);
+                DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+                out.write(params.getBytes(Charset.forName("UTF-8")));
+                out.flush();
+                out.close();
+            }
+            conn.connect();
+            //get result
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String result = convertStreamToString(conn.getInputStream());
+                return result;
+            } else {
+                System.out.println(conn.getResponseCode() + " "+ conn.getResponseMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null)
+                conn.disconnect();
+        }
+        return null;
+    }
+
+
+    public static String convertStreamToString(InputStream is) {
+        /*
+          * To convert the InputStream to String we use the BufferedReader.readLine()
+          * method. We iterate until the BufferedReader return null which means
+          * there's no more data to read. Each line will appended to a StringBuilder
+          * and returned as String.
+          */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
 }
