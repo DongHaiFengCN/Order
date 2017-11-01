3b22c5faff50c9b9c97cf8462d1ceef0fecfdaa9
DOAING <hf.dong@outlook.com>
2017-11-01 12:00:04 +0800

    2017/11/01

---
+++ app/src/main/java/com/zm/order/view/MemberDishesListAdapter.java
@@ -0,0 +1,41 @@
+package com.zm.order.view;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * 项目名称：Order
+ * 类描述：
+ * 创建人：donghaifeng
+ * 创建时间：2017/11/1 10:54
+ * 修改人：donghaifeng
+ * 修改时间：2017/11/1 10:54
+ * 修改备注：
+ */
+
+public class MemberDishesListAdapter extends BaseAdapter {
+    public MemberDishesListAdapter() {
+        super();
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
