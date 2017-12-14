cf72ec2612bf929a13f3cc17b89b3364732b86c0
loongsun@qq.com <loongsun@qq.com>
2017-12-14 13:43:51 +0800

    1130

--- app/src/main/java/application/MyApplication.java
+++ app/src/main/java/application/MyApplication.java
@@ -60,7 +60,6 @@ public class MyApplication extends MobApplication implements ISharedPreferences,
     private final static String DATABASE_NAME = "GYSZDB008";
     private String Company_ID="gysz";
 
-
   //  private final static String SYNCGATEWAY_URL = "blip://123.207.174.171:4984/kitchen/";
     private final static String SYNCGATEWAY_URL = "blip://60.217.194.246:4984/kitchen/";
     private Database database = null;
